bec81aa174e8b5a6c575ed4b8b5c9603783fde12
margo <margo.sockolova2010@yandex.ru>
2018-07-17 16:11:06 +0300

    changa indent from 2 to 4 insode jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     "rules": {
         "func-names": ["error", "never"],
         "spaced-comment": ["error", "always"],
-        "indent": ["error", 4]
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "jsx-quotes": ["error", "prefer-single"],
     }
 };
