7187d5dfb6343ef03efed7220f17e9b02ff17ac2
margo <margo.sockolova2010@yandex.ru>
2018-07-18 15:18:05 +0300

    #1 clean js entry point

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,7 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import registerServiceWorker from './registerServiceWorker';
-import Echo from './Echo/Echo';
-import img from './img/img.jpg';
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Echo>
-                    Victor!
-                </Echo>
-                <img src={img} alt='Neo' />
-            </div>
-        );
-    }
-}
+import App from './app/App';
 
 ReactDOM.render(
     <App />,
