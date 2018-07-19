41e8156f43030d24469c6085a08dfc5830beba81
margo <margo.sockolova2010@yandex.ru>
2018-07-19 19:22:49 +0300

    #1 change jsx quotes accordingly best practise && update npm modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,5 @@ module.exports = {
         "spaced-comment": ["error", "always"],
         "indent": ["error", 4],
         "react/jsx-indent": ["error", 4],
-        "jsx-quotes": ["error", "prefer-single"],
     }
 };
