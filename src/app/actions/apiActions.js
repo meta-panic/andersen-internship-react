701bee9ca9ddb54cfe49f76927eb880a5b67ae5f
margo <margo.sockolova2010@yandex.ru>
2018-07-27 10:10:40 +0300

    #4 ref fetchAnimeInfo function

--- src/app/actions/apiActions.js
+++ src/app/actions/apiActions.js
@@ -1,13 +1,11 @@
 import { actionTypes } from './actionTypes';
 
 function fetchAnimeInfo(ageFilter) {
-  return (dispatch) => {
-    return dispatch({
-      type: actionTypes.API_CALL,
-      url: `filter[ageRating]=${ageFilter}`,
-      requestType: actionTypes.FETCH_ANIME,
-    });
-  };
+  return dispatch => dispatch({
+    type: actionTypes.API_CALL,
+    url: `filter[ageRating]=${ageFilter}`,
+    requestType: actionTypes.FETCH_ANIME,
+  });
 }
 
 export default fetchAnimeInfo;
