6df3dd0a1791a6252e766d301c2bc10cf54dcf43
MaritaMS <m.sokolova@tsconsulting.ru>
2019-07-26 12:04:58 +0300

    add ject testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   "parser": "babel-eslint",
   "env": {
     "browser": true,
-    "node": true
+    "node": true,
+    "jest": true
   },
   "parserOptions": {
     "ecmaVersion": 6,
